b0221ab00b0da468899343724125d61f08fe6c5d
justkmode <136920389+justkmode@users.noreply.github.com>
2024-07-31 14:26:37 +0200

    Update main.tf

--- terraform architecture/main.tf
+++ terraform architecture/main.tf
@@ -1,3 +1,94 @@
 provider "aws" {
   region = "eu-west-2"
 }
+
+# Write
+# Terraform Init
+# Terraform Plan
+# Terraform Apply
+# Terraform Destroy
+
+
+# VPC
+
+
+# Subnet 1
+
+
+# Subnet 2
+
+
+# Internet Gateway
+
+
+# Route Table
+
+
+# Write
+
+
+# VPC
+resource "aws_vpc" "main" {
+  cidr_block = "192.168.0.0/16"
+  
+  tags = {
+    Name = "main-tf-vpc"
+  }
+}
+
+# Subnet 1
+resource "aws_subnet" "subnet1" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "192.168.1.0/24"
+  availability_zone = "us-west-2a"
+  
+  tags = {
+    Name = "subnet-1"
+  }
+}
+
+# Subnet 2
+resource "aws_subnet" "subnet2" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "192.168.2.0/24"
+  availability_zone = "us-west-2b"
+  
+  tags = {
+    Name = "subnet-2"
+  }
+}
+
+# Internet Gateway
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "main-gateway"
+  }
+}
+
+# Route Table
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+  tags = {
+    Name = "public-route-table"
+  }
+}
+
+# Route Table Association for Subnet 1
+resource "aws_route_table_association" "subnet1_association" {
+  subnet_id      = aws_subnet.subnet1.id
+  route_table_id = aws_route_table.public.id
+}
+
+# Route Table Association for Subnet 2
+resource "aws_route_table_association" "subnet2_association" {
+  subnet_id      = aws_subnet.subnet2.id
+  route_table_id = aws_route_table.public.id
+}
