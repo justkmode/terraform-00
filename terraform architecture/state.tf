4be019dd34ffa096630c2bd60a412217ffab8599
justkmode <136920389+justkmode@users.noreply.github.com>
2024-08-03 14:18:58 +0200

    Delete state.tf

--- terraform architecture/state.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "my-terraform-state"
-    key            = "global/s3/terraform.tfstate"
-    region         = "eu-west-2"
-    dynamodb_table = "s3-tf-table"
-  }
-}
